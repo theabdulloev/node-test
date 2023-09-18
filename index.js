869462a348f5e2455797aeda69a14ef15c74ff32
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2023-09-18 14:59:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ app.get('/', function (req, res) {
     res.send('Hello World')
   })
 
+// MONGO DB START
+const { MongoClient } = require('mongodb');
+const url = 'mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/';
+const client = new MongoClient(url);
+const dbName = 'Users';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+// MONGO DB END
+
+
 const port = process.env.PORT || 9001;
 
 app.listen(port,() => console.log(`Listening to port ${port}`));
