b0c8b5c4f2f6c25ab59966c14d0d8090980ee614
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2023-09-18 15:14:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello Friend')
+    res.send('Hello Frien1d')
   })
 
 // MONGO DB START
 const { MongoClient } = require('mongodb');
 const url = 'mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/';
 const client = new MongoClient(url);
-const dbName = 'Friend';
+const dbName = 'TajGram';
 
 async function main() {
   // Use connect method to connect to the server
@@ -19,7 +19,7 @@ async function main() {
   console.log('Connected successfully to server');
   const db = client.db(dbName);
   const collection = db.collection('documents');
-
+const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
   // the following code examples can be pasted here...
 
   return 'done.';
