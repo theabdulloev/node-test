f60f45f3ca76ab550da0e8f7aa800cb5bda5c7fa
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2023-09-19 11:25:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 import express from 'express';
-import crypto from 'crypto';
 import path from 'path';
+import crypto from 'crypto';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { MongoClient } from 'mongodb';
-// const express = require('express');
-// const crypto = require("crypto");
-// const { MongoClient } = require('mongodb');
 const url = 'mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/';
 const client = new MongoClient(url);
 const dbName = 'TagGram';
-
-const app = express();
 app.use(express.json());
-
-
 const jsonParser = express.json();
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(path.join(__dirname))
-app.use('/static', express.static(path.join(__dirname, 'static')))
 
+app.use('/', express.static(path.join(__dirname)));
+
+const port = process.env.PORT || 1313;
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
 app.post("/emailsignup", jsonParser, async (req, res) => {
   await client.connect();
@@ -30,62 +27,22 @@ app.post("/emailsignup", jsonParser, async (req, res) => {
 
 
   if (!req.body) return res.sendStatus(400);
-  const {userLogin,userPassword,userFirstName,userLastName,userEmail} = req.body;
+  const { userLogin, userPassword, userFirstName, userLastName, userEmail } = req.body;
   const hash = crypto.createHmac("sha256", userPassword).digest("hex");
 
   // const filteredDocs = await collection.find({ a: 3 }).toArray();
   // console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
 
   let userProfil = {
-          accauntlogin: userLogin,
-          firstName: userFirstName,
-          lastName: userLastName,
-          telefon: null,
-          email: userEmail,
-          biography: "",
-          password: hash
-        }
+    accauntlogin: userLogin,
+    firstName: userFirstName,
+    lastName: userLastName,
+    telefon: null,
+    email: userEmail,
+    biography: "",
+    password: hash
+  }
 
   const insertResult = await collection.insertOne(userProfil);
   console.log('Inserted documents =>', insertResult);
-
-
-  // await connection.query('SELECT accauntlogin FROM profil WHERE accauntlogin = ?', userLogin, function (error, results, fields) {
-  //   if (error) throw error;
-  //   if (results.length != 0) {
-  //     res.send("имя пользователя занято");
-  //   }
-  //   else {
-  //     let userProfil = {
-  //       id: null,
-  //       accauntlogin: userLogin,
-  //       firstName: userFirstName,
-  //       lastName: userLastName,
-  //       telefon: null,
-  //       email: userEmail,
-  //       biography: "",
-  //       password: hash
-  //     }
-  // //     connection.query('INSERT INTO profil SET ?', userProfil, function (error, results, fields) {
-  // //       if (error) throw error;
-
-  // //       const token = jwt.sign({ userLogin }, 'секретный_ключ');
-  
-  // // // Устанавливаем токен в куку
-  // // res.cookie('token', token);
-
-  // //       res.send(["result",userLogin]);
-
-  // //     });
-  //   }
-  // });
-});
-app.get("/", (req, res) => {
-  res.send(path.join(__dirname));
-});
-
-
-
-const port = process.env.PORT || 1313;
-
-app.listen(port,() => console.log(`Listening to port ${port}`));
+})
