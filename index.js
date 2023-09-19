1db9e9a3963f8e491cb6b2f120de7ecdc8086975
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2023-09-19 08:34:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,83 @@
 const express = require('express');
+const crypto = require("crypto");
+const { MongoClient } = require('mongodb');
+const url = 'mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/';
+const client = new MongoClient(url);
+const dbName = 'TagGram';
 
 const app = express();
 app.use(express.json());
 
+
+const jsonParser = express.json();
+app.use(express.static(__dirname + "/static"));
+
+
+app.post("/emailsignup", jsonParser, async (req, res) => {
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection('USERS');
+
+
+
+  if (!req.body) return res.sendStatus(400);
+  const {userLogin,userPassword,userFirstName,userLastName,userEmail} = req.body;
+  const hash = crypto.createHmac("sha256", userPassword).digest("hex");
+
+  // const filteredDocs = await collection.find({ a: 3 }).toArray();
+  // console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
+
+  let userProfil = {
+          accauntlogin: userLogin,
+          firstName: userFirstName,
+          lastName: userLastName,
+          telefon: null,
+          email: userEmail,
+          biography: "",
+          password: hash
+        }
+
+  const insertResult = await collection.insertOne(userProfil);
+  console.log('Inserted documents =>', insertResult);
+
+
+  // await connection.query('SELECT accauntlogin FROM profil WHERE accauntlogin = ?', userLogin, function (error, results, fields) {
+  //   if (error) throw error;
+  //   if (results.length != 0) {
+  //     res.send("имя пользователя занято");
+  //   }
+  //   else {
+  //     let userProfil = {
+  //       id: null,
+  //       accauntlogin: userLogin,
+  //       firstName: userFirstName,
+  //       lastName: userLastName,
+  //       telefon: null,
+  //       email: userEmail,
+  //       biography: "",
+  //       password: hash
+  //     }
+  // //     connection.query('INSERT INTO profil SET ?', userProfil, function (error, results, fields) {
+  // //       if (error) throw error;
+
+  // //       const token = jwt.sign({ userLogin }, 'секретный_ключ');
+  
+  // // // Устанавливаем токен в куку
+  // // res.cookie('token', token);
+
+  // //       res.send(["result",userLogin]);
+
+  // //     });
+  //   }
+  // });
+});
+
+
 app.get('/', function (req, res) {
-    res.send('Hello Frien1d')
+    res.send('Hello World')
   })
 
-// MONGO DB START
-const { MongoClient } = require('mongodb');
-const url = 'mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/';
-const client = new MongoClient(url);
-const dbName = 'TajGram';
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
-const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
-  // the following code examples can be pasted here...
-
-  return 'done.';
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
-// MONGO DB END
 
 
 const port = process.env.PORT || 9001;
